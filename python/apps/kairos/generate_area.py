ca1dc03733018f2224336f3638c300cb5837f5bc
Edgar Cristobal Ramirez Miranda <edgar.ramirez@dormakaba.com>
2022-10-26 10:51:42 -0400

    typos and format

--- python/apps/kairos/generate_area.py
+++ python/apps/kairos/generate_area.py
@@ -7,6 +7,7 @@ global y3
 global x4
 global y4
 
+
 def int_positive(value):
     try:
         value_int = int(value)
@@ -20,6 +21,7 @@ def int_positive(value):
 
     return value_int
 
+
 def set_x1():
     global x1
     while 1:
@@ -28,6 +30,7 @@ def set_x1():
         if x1 is False:
             continue
         break
+
 
 def set_y1():
     global y1
@@ -38,11 +41,12 @@ def set_y1():
             continue
         break
 
+
 def set_x2():
-    global x2,y1,y2
+    global x2, y1, y2
     while 1:
         x2 = input("Provide only x2, y2 will be equal to y1: ")
-        x2 =  int_positive(x2)
+        x2 = int_positive(x2)
         if x2 is False:
             continue
         y2 = y1
@@ -61,8 +65,9 @@ def set_x3():
             continue
         break
 
+
 def set_y3():
-    global y1,y3
+    global y1, y3
     while 1:
         y3 = input("Provide y3: ")
         y3 = int_positive(y3)
@@ -72,9 +77,10 @@ def set_y3():
             print("y3 should be a value at least 2 greater or 2 lesser than y1")
             continue
         break
-    
+
+
 def set_x4():
-    global x3,y3,x4,y4
+    global x3, y3, x4, y4
     while 1:
         x4 = input("Provide only x4, y4 will be equal to y3: ")
         x4 = int_positive(x4)
@@ -83,29 +89,33 @@ def set_x4():
         if abs(x4-x3) < 2:
             print("x4 should be a value at least 2 greater or 2 lesser than x3")
             continue
-        if x4>(x3-2):
+        if x4 > (x3-2):
             print("Wrong x4 coordinates, lines could not cross each other")
             continue
         y4 = y3
         break
 
-#x's mayores que 2 para garantizar que existe un area en el cuadrilatero
-#toda coordenada es diferente de (0,0)
 
 def coordinates():
+    """
+    x's mayores que 2 para garantizar que existe un area en el cuadrilatero
+    toda coordenada es diferente de (0,0)
+    :return:
+    """
     set_x1()
     set_y1()
-    print("({},{})".format(x1,y1))
+    print("({},{})".format(x1, y1))
     set_x2()
-    print("({},{})".format(x2,y2))
+    print("({},{})".format(x2, y2))
     set_x3()
     set_y3()
-    print("({},{})".format(x3,y3))
+    print("({},{})".format(x3, y3))
     set_x4()
 
-    print("({},{})".format(x1,y1))
-    print("({},{})".format(x2,y2))
-    print("({},{})".format(x3,y3))
-    print("({},{})".format(x4,y4))
+    print("({},{})".format(x1, y1))
+    print("({},{})".format(x2, y2))
+    print("({},{})".format(x3, y3))
+    print("({},{})".format(x4, y4))
+
 
 coordinates()
